bf20e793d81bdcdf18ba3ca5aeb3cda143a00579
Yuri <yuri.kuniver@gmail.com>
2022-08-17 14:46:10 +0300

    First draft added

---
+++ test_main_page.py
@@ -0,0 +1,13 @@
+import time
+
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
